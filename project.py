f2e084afb3197a3f8139e238eed5dbe9269a060c
AlexBindevald <alexbindevald@gmail.com>
2019-04-25 20:15:21 +0300

    Update project.py

--- project.py
+++ project.py
@@ -7,21 +7,44 @@ import re
 query_string = urllib.parse.urlencode({"search_query" : input('Enter name:')})
 html_content = urllib.request.urlopen("http://www.youtube.com/results?" + query_string)
 search_results = re.findall(r'href=\"\/watch\?v=(.{11})', html_content.read().decode())
-ydl_opts = {
-    'outtmpl': '%(title)s' + '.mp3',
-    'restrictfilenames': 'true'
-}
 
-video = "http://www.youtube.com/watch?v=" + search_results[5]
-with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-    info = ydl.extract_info(video, download=False)
-    url = info.get('url', None)
-    id = info.get('id', None)
-    title = info.get('title', None)
+mylist = list(dict.fromkeys(search_results))
+musicUrl = []
+musicNames = []
 
-print(title)
-title = title.replace(' ', '_')
-file = str(title) + '.mp3'
-os.system(file)
+for i in mylist:
+    ydl_opts = {
+        'quiet': True
+    }
+    video = "http://www.youtube.com/watch?v=" + i
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        info = ydl.extract_info(video, download=False)
+        title = info.get('title', None)
+        musicUrl.append(i)
+        musicNames.append(title)
+for i in range(len(musicNames)):
+    print(str(i+1) + '. ' + musicNames[i] )
+try:
+    number = int(input('What song would you like to listen(Enter number before the song name):'))
+except:
+    print('Wrong')
+def download(videoUrl):
+    video = "http://www.youtube.com/watch?v=" + videoUrl
+    ydl_opts = {
+        'outtmpl': 'Song' + '.mp3',
+        'restrictfilenames': 'true'
+    }
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([video])
+        info = ydl.extract_info(video, download=False)
+        title = info.get('title', None)
 
+    file = 'Song.mp3'
+    return file
+
+def play_music(file):
+    os.system(file)
+
+
+play_music(download(musicUrl[number - 1]))
 
